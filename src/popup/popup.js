e036bcc3364ae69c9c2404518423bc1dc9ffc015
Den <denis.inozemtsev@btb.dev>
2023-06-11 21:03:56 +0400

    remove on-click-outside

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -3,21 +3,7 @@ import App from './App.vue'
 
 global.browser = require('webextension-polyfill')
 Vue.prototype.$browser = global.browser
-Vue.directive('click-outside', {
-  bind: function (el, binding, vnode) {
-    el.clickOutsideEvent = function (event) {
-      // here I check that click was outside the el and his children
-      if (!(el === event.target || el.contains(event.target))) {
-        // and if it did, call method provided in attribute value
-        vnode.context[binding.expression](event)
-      }
-    }
-    document.body.addEventListener('click', el.clickOutsideEvent)
-  },
-  unbind: function (el) {
-    document.body.removeEventListener('click', el.clickOutsideEvent)
-  }
-})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
